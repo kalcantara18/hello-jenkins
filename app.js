0d8379282e58e181a21526ad4a7a2d4de02d0d6d
Karla Alcántara García <kalcantara@casystem.com.mx>
2023-03-24 12:29:18 -0600

    se actualiza archivo js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('hello world!');
+  res.send('hello world');
+  response.end("Hello World!");
 });
 app.listen(process.env.PORT || 5000);
 module.exports = app;
